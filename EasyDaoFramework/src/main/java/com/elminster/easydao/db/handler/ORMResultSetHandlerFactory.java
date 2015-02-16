182dec09f7d10880fc09daabfc64400b56f8ad42
JGu <Jinglei.Gu@kisters.cn>
2015-02-16 14:11:12 +0800

    +using TypeUtil to get the generic type in List.

--- EasyDaoFramework/src/main/java/com/elminster/easydao/db/handler/ORMResultSetHandlerFactory.java
+++ EasyDaoFramework/src/main/java/com/elminster/easydao/db/handler/ORMResultSetHandlerFactory.java
@@ -1,9 +1,10 @@
 package com.elminster.easydao.db.handler;
 
 import java.lang.reflect.Method;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.util.List;
+
+import com.elminster.common.util.CollectionUtil;
+import com.elminster.common.util.TypeUtil;
 
 public class ORMResultSetHandlerFactory {
 
@@ -16,33 +17,20 @@ public class ORMResultSetHandlerFactory {
 	}
 	
 	public IResultSetHandler getResultSetHandler(Method invokeMethod, Class<?> originalClass) throws Exception {
-		Class<?> returnClazz = invokeMethod.getReturnType();
+	  List<Class<?>> returnClasses = TypeUtil.INSTANCE.getMethodReturnTypeClass(originalClass, invokeMethod);
 		IResultSetHandler resultSetHandler = null;
-		if (null == returnClazz) {
+		if (CollectionUtil.isEmpty(returnClasses)) {
 			return null;
-		} else if (List.class.isAssignableFrom(returnClazz)) {
+		} 
+		Class<?> returnClazz = returnClasses.get(0);
+		if (List.class.isAssignableFrom(returnClazz)) {
 			// List
-			ParameterizedType pt = (ParameterizedType) invokeMethod.getGenericReturnType();
-			String typeArguments[] = pt.getActualTypeArguments()[0].toString().split(" ");
-			String className = null;
-			if (typeArguments.length > 1) {
-			  className = typeArguments[1];
-			} else {
-			  Type[] types = originalClass.getGenericInterfaces();
-			  if (types.length > 0) {
-			    typeArguments = ((ParameterizedType)types[0]).getActualTypeArguments()[0].toString().split(" ");
-			    if (typeArguments.length > 1) {
-		        className = typeArguments[1];
-			    }
-			  }
-			}
-			if (null == className) {
-			  throw new NullPointerException("cannot found generic type for class: " + originalClass.getName());
-			}
-			Class<?> clazz = Class.forName(className);
+			Class<?> genericClass = returnClasses.get(1);
+			Class<?> clazz = Class.forName(genericClass.getName());
 			resultSetHandler = new ORMListResultSetHandler(clazz);
 		} else if (returnClazz.isArray()) {
 			// Array
+		  resultSetHandler = new ORMArrayResultSetHandler(returnClazz);
 		} else {
 			resultSetHandler = new ORMObjectResultSetHandler(returnClazz);
 		}
